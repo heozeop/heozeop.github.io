e095dff084e119fe0b7f449ae30e1fa23b8f8b83
Lee seung chan <dltmdcks702@gmail.com>
2020-12-23 21:32:33 +0900

    리팩토링: seo 컴포넌트 tsx로 리팩토링

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -9,8 +9,9 @@ import React from "react"
 import PropTypes from "prop-types"
 import { Helmet } from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import { SEOType } from 'src/types/seo'
 
-const SEO = ({ description, lang, meta, title }) => {
+const SEO = ({ description, lang, meta, title } : SEOType) => {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -36,7 +37,7 @@ const SEO = ({ description, lang, meta, title }) => {
         lang,
       }}
       title={title}
-      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : undefined}
       meta={[
         {
           name: `description`,
---
+++ src/types/seo.ts
@@ -0,0 +1,6 @@
+export class SEOType {
+  description!: string;
+  lang!: string;
+  meta!: ConcatArray<{ name: string; content: any; property?: undefined; } | { property: string; content: any; name?: undefined; }>;
+  title!: string;
+}
