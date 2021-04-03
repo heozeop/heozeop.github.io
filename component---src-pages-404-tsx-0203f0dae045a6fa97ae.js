4ae6fde840a29f355d9809683215af199c766759
Lee seung chan <dltmdcks702@gmail.com>
2021-04-03 23:02:01 +0900

    Updates

---
+++ component---src-pages-404-tsx-0203f0dae045a6fa97ae.js
@@ -0,0 +1,2 @@
+(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[218],{9786:function(t,e,a){"use strict";a.r(e);var n=a(7294),l=a(6722),s=a(8972);e.default=function(t){var e=t.data,a=t.location,o=e.site.siteMetadata.title;return n.createElement(l.Z,{location:a,title:o},n.createElement(s.Z,{title:"404: Not Found"}),n.createElement("h1",null,"404: Not Found"),n.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-0203f0dae045a6fa97ae.js.map
